36c18e35ebc0db38cf9d734b2d46f7c543560123
phamducdat <phamducdat2402@gmail.com>
2023-04-23 16:31:33 +0700

    update: sider

--- src/components/sider/index.tsx
+++ src/components/sider/index.tsx
@@ -1,7 +1,27 @@
 import React from "react";
-import {SiderProps} from "antd";
+import {Menu, MenuProps, SiderProps} from "antd";
 import Sider from "antd/es/layout/Sider";
+import {LaptopOutlined, NotificationOutlined, UserOutlined} from "@ant-design/icons";
 
+const items2: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
+    (icon, index) => {
+        const key = String(index + 1);
+
+        return {
+            key: `sub${key}`,
+            icon: React.createElement(icon),
+            label: `subnav ${key}`,
+
+            children: new Array(4).fill(null).map((_, j) => {
+                const subKey = index * 4 + j + 1;
+                return {
+                    key: subKey,
+                    label: `option${subKey}`,
+                };
+            }),
+        };
+    },
+);
 
 export interface CustomSiderProps extends SiderProps {
 
@@ -13,7 +33,15 @@ const CustomSider: React.FC<CustomSiderProps> = props => {
         <>
             <Sider
                 {...props}
-            />
+            >
+                <Menu
+                    mode="inline"
+                    defaultSelectedKeys={['1']}
+                    defaultOpenKeys={['sub1']}
+                    style={{ height: '100%', borderRight: 0 }}
+                    items={items2}
+                />
+            </Sider>
         </>
     );
 };
