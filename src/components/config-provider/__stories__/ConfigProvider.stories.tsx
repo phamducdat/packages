51f65998ba9ad81e8fe8c03d464044a0124015e7
phamducdat <phamducdat2402@gmail.com>
2023-05-05 16:31:56 +0700

    chore: add data, columns to table

--- src/components/config-provider/__stories__/ConfigProvider.stories.tsx
+++ src/components/config-provider/__stories__/ConfigProvider.stories.tsx
@@ -3,6 +3,38 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import Table from '../../table';
 import React from 'react';
 
+const dataSource = [
+  {
+    key: '1',
+    name: 'Mike',
+    age: 32,
+    address: '10 Downing Street',
+  },
+  {
+    key: '2',
+    name: 'John',
+    age: 42,
+    address: '10 Downing Street',
+  },
+];
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+];
 export default {
   title: 'Config Provider',
   component: ConfigProvider,
@@ -11,7 +43,7 @@ export default {
 
 const Template: ComponentStory<typeof ConfigProvider> = (args) => (
   <ConfigProvider {...args}>
-    <Table />
+    <Table dataSource={dataSource} columns={columns} />
   </ConfigProvider>
 );
 
