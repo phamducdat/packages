961445c159f2fdba73ed3267803cb9ce95b615f0
phamducdat <phamducdat2402@gmail.com>
2023-05-05 17:36:47 +0700

    refactor: using params to pagination

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -1,15 +1,45 @@
-import React, { useContext } from 'react';
-import { Table as AntTable, TableProps as AntTableProps } from 'antd';
+import React, { useContext, useEffect, useState } from 'react';
+import {
+  Table as AntTable,
+  TablePaginationConfig,
+  TableProps as AntTableProps,
+} from 'antd';
 import { ConfigContext } from '../config-provider/ConfigContext';
+import { useLocation, useSearchParams } from 'react-router-dom';
 
 export type TableProps = AntTableProps<any>;
 
 const Table: React.FC<TableProps> = (props) => {
   const config = useContext(ConfigContext);
   const params = config.table?.params ?? {};
+  const defaultParams = {
+    pageText: 'page',
+    pageSizeText: 'pageSize',
+    sortText: 'sort',
+  };
+  const { pageText, pageSizeText, sortText } = { ...defaultParams, ...params };
+  const [searchParams] = useSearchParams();
+  const [paginationData, setPaginationData] = useState();
+  const location = useLocation();
+
+  const pagination: TablePaginationConfig = {
+    ...props.pagination,
+    defaultCurrent: parseInt(searchParams.get(pageText) ?? '1', 10),
+    defaultPageSize: parseInt(searchParams.get(pageSizeText) ?? '1', 10),
+  };
+
+  useEffect(() => {
+    console.log('dat with search params = ', searchParams.get(pageText));
+  }, [searchParams]);
+
+  useEffect(() => {
+    console.log('dat with location, params = ', location, searchParams);
+  }, []);
+
+  console.log('dat with pagination = ', pagination);
   return (
     <div>
-      <AntTable {...props} />
+      <AntTable {...props} pagination={pagination} />
     </div>
   );
 };
