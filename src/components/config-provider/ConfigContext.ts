5ed0eafc217adc97ff91655c772884e2d7c5ef04
phamducdat <phamducdat2402@gmail.com>
2023-05-05 16:27:52 +0700

    feat(Table): using ConfigProviderContext in Table

--- src/components/config-provider/ConfigContext.ts
+++ src/components/config-provider/ConfigContext.ts
@@ -1,13 +1,25 @@
 import { createContext } from 'react';
 
+export interface TableConfig {
+  params?: {
+    pageText?: string;
+    pageSizeText?: string;
+    sortText?: string;
+  };
+}
+
 export interface Config {
-  them?: string;
-  language?: string;
+  table?: TableConfig;
 }
 
 const defaultConfig: Config = {
-  them: 'light',
-  language: 'en',
+  table: {
+    params: {
+      sortText: 'sort',
+      pageSizeText: 'pageSize',
+      pageText: 'page',
+    },
+  },
 };
 
 export const ConfigContext = createContext<Config>(defaultConfig);
--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Table as AntTable, TableProps as AntTableProps } from 'antd';
+import { ConfigContext } from '../config-provider/ConfigContext';
 
 export type TableProps = AntTableProps<any>;
 
 const Table: React.FC<TableProps> = (props) => {
+  const config = useContext(ConfigContext);
+  const params = config.table?.params ?? {};
   return (
     <div>
       <AntTable />
