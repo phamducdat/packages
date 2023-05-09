5b184c0f3a71a62c4e1cf95ff42e0ddb445b1311
phamducdat <phamducdat2402@gmail.com>
2023-05-09 11:32:44 +0700

    refactor(Tabs): omit activeKey in AntTabsProps

--- src/components/tabs/index.tsx
+++ src/components/tabs/index.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import { Tabs as AntTabs, TabsProps as AntTabsProps } from 'antd';
+import { useSearchParams } from 'react-router-dom';
 
-export type TabsProps = AntTabsProps;
-const Tabs: React.FC<TabsProps> = (props) => {
+export type TabsProps = Omit<AntTabsProps, 'activeKey'>;
+
+const Tabs: React.FC<TabsProps> = ({ onChange, ...restProps }) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleOnChange = (value: string) => {
+    const currentSearchParams = new URLSearchParams(searchParams.toString());
+    currentSearchParams.set('tab-key', value);
+    setSearchParams(currentSearchParams);
+
+    if (onChange) onChange(value);
+  };
+
   return (
     <div>
-      <AntTabs />
+      <AntTabs
+        {...restProps}
+        activeKey={searchParams.get('tab-key') ?? undefined}
+        key={searchParams.get('tab-key') !== null ? 'dp' : 'test'}
+        onChange={handleOnChange}
+      />
     </div>
   );
 };
